37455cabd0e0bd1d88af27d09e0678cc74ddc61a
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2020-02-18 15:21:02 -0500

    Now displaying the card name

--- src/App.js
+++ src/App.js
@@ -6,6 +6,13 @@
    render () {
      return (
        //JSX to render here..
+       <div class="card">
+       <div class="card-body">
+         <h5 class="card-title">Steve Jobs</h5>
+         <h6 class="card-subtitle mb-2 text-muted">steve@apple.com</h6>
+         <p class="card-text">Stay Hungry, Stay Foolish</p>
+       </div>
+     </div>
      );
    }
  }
