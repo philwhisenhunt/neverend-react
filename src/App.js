2aa66ff6e2a7fe518b48873a3af2b3960f0d3cbb
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2020-02-19 20:27:15 -0500

    Rewrote by hand to learn syntax differences

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
- // src/App.js
+import React, { Component } from 'react';
 
- import React, {Component} from 'react';
+class App extends Component {
 
- class App extends Component {
-   render () {
-     return (
-       //JSX to render here..
-       <div class="card">
-       <div class="card-body">
-         <h5 class="card-title">Steve Jobs</h5>
-         <h6 class="card-subtitle mb-2 text-muted">steve@apple.com</h6>
-         <p class="card-text">Stay Hungry, Stay Foolish</p>
-       </div>
-     </div>
-     );
-   }
- }
+  state = {
+    contacts: []
+  }
+  ...
+}
 
- export default App;
+import React, { Component } from 'react';
+
+class App extends Component {
+ 
+  componentDidMount() {
+    fetch('http://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then((data) => {
+      this.setState({ contacts: data })
+    })
+    .catch(console.log)
+  }
+}
