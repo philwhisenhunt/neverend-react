4f429dd0c5604a515334f42dfe17d11fb7aa8b42
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2020-02-20 13:05:02 -0500

    Loading file, but not correctly (all wacky)

--- src/components/mainline.js
+++ src/components/mainline.js
@@ -3,7 +3,23 @@ import React from 'react'
 const Mainline = ({ instructions }) => {
     return (
       <div>
-        <center><h1>Mainline</h1></center>
+            <div class="navigator">Endless</div>
+                <div class="hero-image" style={{ backgroundImage: `url(require("/images/photo-couch_2x.jpg"))` }}>
+                    <div class="hero-text">
+                        <h4>New Games & Accessories</h4>
+                        <h1>Monthly Packages</h1>
+                        <h1>Excitement Delivered Daily</h1>
+
+                    <p>What’s the best way to shop for the latest video games and peripherals? How about never shopping at all? You'll get new stuff on your doorstep - every month.</p>
+                    <button id="get-started">Get started</button>
+                    </div>
+                </div>
+
+                <div class="step-holder">
+                    <h3 id="how-it-works">How it Works</h3>
+
+
+        </div>
        
       </div>
     )
