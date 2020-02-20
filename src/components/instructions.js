6ceb21db3ad9aeec4d5467712dd66d9543da5934
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2020-02-19 22:42:25 -0500

    Wrapping up, has some stuff missing when getting title. Just delete line 12 if needed

--- src/components/instructions.js
+++ src/components/instructions.js
@@ -1,14 +1,20 @@
 import React from 'react'
 
 const Instructions = ({ instructions }) => {
-    return(
-        <div>
-            <center><h1>Instructions</h1></center>
-            {instructions.map((instruction) => (
-                <p>{instruction.id}</p>
-            ))}
-        </div>
-    )
-}
+  return (
+    <div>
+      <center><h1>Contact List</h1></center>
+      {instructions.map((instruction) => (
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">{instruction.id}</h5>
+            <h6 class="card-subtitle mb-2 text-muted">{instruction.stepNumber}</h6>
+            <p class="card-text">{instruction.versionedContent.title}</p>
+          </div>
+          </div>
+      ))}
+    </div>
+  )
+};
 
 export default Instructions
