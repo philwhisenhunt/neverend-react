ddbe114f71fd5a1af3d78cfe919bbb536fb18946
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2020-02-20 15:18:45 -0500

    Moved everything inside the blue box, now to make them into columns

--- src/components/instructions.js
+++ src/components/instructions.js
@@ -2,14 +2,17 @@ import React from 'react'
 
 const Instructions = ({ instructions }) => {
   return (
-    <div>
+    <div class="blue-box">
       <center><h1>How it Works</h1></center>
       {instructions.sort(
           function(a, b){
               return a.stepNumber - b.stepNumber;
           }
       ).map((instruction) => (
+
         <div class="card">
+                  
+
           <div class="card-body">
             <h5 class="card-title">{instruction.id}</h5>
             <h6 class="card-subtitle mb-2 text-muted">{instruction.stepNumber}</h6>
@@ -19,6 +22,7 @@ const Instructions = ({ instructions }) => {
 
           </div>
           </div>
+          
       ))}
     </div>
   )
