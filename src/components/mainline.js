49ecdf9860574800f4bc2433fcea0d3ed590e86d
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2020-02-25 20:18:19 -0500

    Redoing front page styling to make more mobile friendly, this version has green background to prevent sharing for now

--- src/components/mainline.js
+++ src/components/mainline.js
@@ -8,14 +8,15 @@ const Mainline = ({ instructions }) => {
             <div class="navigator">Endless</div>
                 <div class="hero-image">
                     <img src={photoz} class="hero-image"/>
+                    <div class="hero-text-container"> 
+                      <div class="hero-text">
+                          <h4>New Games & Accessories</h4>
+                          <h1>Monthly Packages</h1>
+                          <h1>Excitement Delivered Daily</h1>
 
-                    <div class="hero-text">
-                        <h4>New Games & Accessories</h4>
-                        <h1>Monthly Packages</h1>
-                        <h1>Excitement Delivered Daily</h1>
-
-                    <p>What’s the best way to shop for the latest video games and peripherals? How about never shopping at all? You'll get new stuff on your doorstep - every month.</p>
-                    <button id="get-started">GET STARTED</button>
+                      <p>What’s the best way to shop for the latest video games and peripherals? How about never shopping at all? You'll get new stuff on your doorstep - every month.</p>
+                      <button id="get-started">GET STARTED</button>
+                      </div>
                     </div>
                 </div>
        
